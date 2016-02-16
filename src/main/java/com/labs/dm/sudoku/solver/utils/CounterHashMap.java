d28a3fad292252bb43dac0031d33acd9f9602ab2
unknown <daniel.mroczka@gmail.com>
2016-02-16 17:05:05 +0100

    Implement Reduction Algorithm

---
+++ src/main/java/com/labs/dm/sudoku/solver/utils/CounterHashMap.java
@@ -0,0 +1,7 @@
+package com.labs.dm.sudoku.solver.utils;
+
+/**
+ * Created by daniel on 2016-02-15.
+ */
+public class CounterHashMap {
+}
---
+++ src/test/java/com/labs/dm/sudoku/solver/alg/ReductionTest.java
@@ -0,0 +1,16 @@
+package com.labs.dm.sudoku.solver.alg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by daniel on 2016-02-15.
+ */
+public class ReductionTest {
+
+    @Test
+    public void execute() throws Exception {
+
+    }
+}
