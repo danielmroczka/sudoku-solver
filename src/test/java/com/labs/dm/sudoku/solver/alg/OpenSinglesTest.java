3fde895b4ab53588e26416ea88c008a20557e05b
Daniel Mroczka <daniel.mroczka@gmail.com>
2023-11-09 19:43:06 +0100

    wip

--- src/test/java/com/labs/dm/sudoku/solver/alg/OpenSinglesTest.java
+++ src/test/java/com/labs/dm/sudoku/solver/alg/OpenSinglesTest.java
@@ -40,6 +40,14 @@ public class OpenSinglesTest {
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, input);
     }
 
+
+    @Test
+    public void testOpenSingleFilledArrayInvalid() {
+        int[] input = new int[]{0, 1, 1, 4, 5, 6, 7, 8, 9};
+        assertEquals(-1, singles.fillOpenSingles(input));
+        assertArrayEquals(new int[]{0, 1, 1, 4, 5, 6, 7, 8, 9}, input);
+    }
+
     @Test
     public void testOpenSingleFilledArrayInvalid() {
         int[] input = new int[]{0, 1, 1, 4, 5, 6, 7, 8, 9};
